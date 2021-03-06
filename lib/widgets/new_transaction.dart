614e7db8478acaed7c839b37419e0920c284062f
eng.mahmoudeid <eng.mahmoudeid@gmail.com>
2021-03-06 23:49:47 +0400

    convert new transaction widget to StatefullWidget

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function addTransactionHandler;
   NewTransaction(this.addTransactionHandler);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
+
   void submitData() {
     final enteredTitle = titleController.text;
     final enteredAmount = double.parse(amountController.text);
     if (enteredTitle.isEmpty || enteredAmount <= 0) {
       return;
     }
-    addTransactionHandler(enteredTitle, enteredAmount);
+    widget.addTransactionHandler(enteredTitle, enteredAmount);
+    Navigator.of(context).pop(); // to close window
   }
 
   @override
